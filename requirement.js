aa1e2e00573eee2396b48db0f42804b77823e67d
tejasrane121 <98016526+tejasrane121@users.noreply.github.com>
2022-01-19 15:25:41 +0530

    Add files via upload

---
+++ requirement.js
@@ -0,0 +1,45 @@
+// require for our project
+
+// we are a book management company
+
+//books
+//isbn,title,pub date,lang,num page,author[],cat[]
+
+
+//authors
+//id,name,books[]
+
+
+//publication
+//id,name,books[]
+
+
+      //we have to design and code an api over this
+
+//1.books
+//we need an api
+//to get all the books
+//to get a list of books based on cat..
+//to get a list of books based on lang..
+
+
+//2.Author
+//We need an API :-
+//To get all the authors
+//To get a specific author
+//To get a list of authors based on books
+
+
+//3.publication
+//We need an API:-
+//To get all the publications
+//To get a specific Author
+//To get a specific publication
+
+
+//Schema - Blueprint of how data has to be constructed
+//MongoDB is schemaless
+//mongoose has schema
+//mongoose - validation , relationship with other data.
+//model -> document model of MongoDB
+//Schema -> Model -> use them .
